ec91343e9ac35b3d2cf49755bf8176413ebc79a7
louisj99 <louis.stockton@hotmail.co.uk>
2023-09-09 23:02:13 +0100

    added header and sidebar

---
+++ svelte-components/src/components/header.svelte
@@ -0,0 +1,60 @@
+<!-- Header.svelte -->
+<script>
+    // Define the navigation links for your top bar
+    const navigationLinks = [
+        { label: 'Home', route: '/' },
+        { label: 'About', route: '/about' },
+        { label: 'Services', route: '/services' },
+        // Add more navigation links as needed
+    ];
+
+    // Function to handle link clicks
+    function handleLinkClick(route) {
+        // You can implement the navigation logic here
+        console.log(`Navigating to ${route}`);
+    }
+</script>
+
+<header class="top-bar">
+    <nav class="navigation">
+        <ul>
+            {#each navigationLinks as link (link.label)}
+                <li on:click={() => handleLinkClick(link.route)}>
+                    <a href="javascript:void(0)">{link.label}</a>
+                </li>
+            {/each}
+        </ul>
+    </nav>
+</header>
+
+<style>
+    /* Add your top bar styling here */
+    .top-bar {
+        background-color: #333;
+        color: #fff;
+        padding: 10px 20px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .navigation ul {
+        list-style: none;
+        padding: 0;
+        display: flex;
+    }
+
+    .navigation li {
+        margin-right: 20px;
+        cursor: pointer;
+    }
+
+    .navigation a {
+        text-decoration: none;
+        color: #fff;
+    }
+
+    .navigation a:hover {
+        text-decoration: underline;
+    }
+</style>
---
+++ svelte-components/src/components/sidebar.svelte
@@ -0,0 +1,58 @@
+<!-- Sidebar.svelte -->
+<script>
+    // Define the items in your sidebar
+    const sidebarItems = [
+        { label: 'Home', icon: 'home', route: '/' },
+        { label: 'About', icon: 'info', route: '/about' },
+        { label: 'Services', icon: 'settings', route: '/services' },
+        // Add more sidebar items as needed
+    ];
+
+    // Function to handle item click
+    function handleItemClick(route) {
+        // You can implement the navigation logic here
+        console.log(`Navigating to ${route}`);
+    }
+</script>
+
+<nav class="sidebar">
+    <ul>
+        {#each sidebarItems as item (item.label)}
+            <li on:click={() => handleItemClick(item.route)}>
+                <i class="material-icons">{item.icon}</i>
+                <span>{item.label}</span>
+            </li>
+        {/each}
+    </ul>
+</nav>
+
+<style>
+    /* Add your sidebar styling here */
+    .sidebar {
+        width: 250px;
+        background-color: #333;
+        color: #fff;
+        padding: 20px;
+    }
+
+    ul {
+        list-style: none;
+        padding: 0;
+    }
+
+    li {
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+    }
+
+    li:hover {
+        background-color: #555;
+    }
+
+    i {
+        margin-right: 10px;
+    }
+</style>

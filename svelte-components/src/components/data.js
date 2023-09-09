f236a59240589c068b92e9f507c9cecc7f220b8c
louisj99 <louis.stockton@hotmail.co.uk>
2023-09-09 22:59:21 +0100

    first commit

---
+++ svelte-components/src/components/data.js
@@ -0,0 +1,8 @@
+// data.js
+const data = [
+    { name: 'John Doe', age: 30 },
+    { name: 'Jane Smith', age: 25 },
+    // Add more data rows as needed
+];
+
+export default data;
---
+++ svelte-components/src/components/table.svelte
@@ -0,0 +1,17 @@
+<!-- Table.svelte -->
+<script>
+    // Import the necessary components and data
+    import { Table, Column } from 'svelte-table';
+    import data from './data'; // Your data source
+
+    // Define the columns for your table
+    const columns = [
+        new Column('Name', 'name'), // Replace 'name' with the actual field name from your data
+        new Column('Age', 'age'),   // Replace 'age' with the actual field name from your data
+        // Add more columns as needed
+    ];
+</script>
+
+<Table {columns} {data}>
+    <!-- You can add any custom table UI here, e.g., pagination -->
+</Table>
